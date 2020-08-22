02c808eb9c22cf1e449aaca430ac6e6c51a6eaba
Will Lytle <wlytle89@gmail.com>
2020-08-22 23:40:13 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,59 @@
-# Add your code here
+def welcome
+  puts "Please enter a command:" 
+  gets.strip
+end
+
+def help
+puts "I accept the following commands:
+- help : displays this help message
+- list : displays a list of songs you can play
+- play : lets you choose a song to play
+- exit : exits this program"
+end
+
+def list(songs) 
+  songs.each_with_index do |song, index|
+    puts "#{index + 1}. #{song}"
+  end
+end
+
+def play(songs)
+  puts "please enter a song name or number:"
+  selected_song = gets.strip
+  
+  songs.each_with_index do |song, index|
+    index += 1
+    message = "Invalid input, please try again"
+    if selected_song == song || 
+      selected_song == index.to_s
+      message = "Playing #{song}"
+    end
+  end
+  puts message
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(songs)
+  exit = false
+  command = welcome
+  until exit == true
+    case command
+    when "help"
+      help
+    when "list"
+      list(songs)
+    when "play"
+      play(songs)
+    when "exit"
+      exit_jukebox
+      exit = true
+    else
+      puts "#{command} is not a valid command."
+    end
+  end
+end
+
+
